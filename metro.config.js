fcdf1eb8116f132499023bea119c591e7658ace6
alhsryahmd266-jpg <alhsryahmd266@gmail.com>
2026-07-08 12:36:50 +0300

    Fix metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,23 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
-
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
-
-const config = getDefaultConfig(projectRoot);
-
-config.watchFolders = [workspaceRoot];
-
+const config = getDefaultConfig(__dirname);
 config.resolver = {
   ...config.resolver,
-  nodeModulesPaths: [
-    path.resolve(projectRoot, "node_modules"),
-    path.resolve(workspaceRoot, "node_modules"),
-  ],
-  blockList: [
-    // Block pnpm temp dirs (_tmp_XXXX) created during package installs
-    /.*_tmp_\d+.*/,
-  ],
+  blockList: [/.*_tmp_\d+.*/],
 };
-
 module.exports = config;
